4b5fa40e89ce406669cfa4cf19fc04e4ae3b7742
jacob111764 <jacob111764@mcpsva.org>
2026-09-01 15:02:29 -0400

    .

---
+++ madlib.py
@@ -0,0 +1,50 @@
+"""
+Filename: madlibs.py
+Author: <Melendez, Jacob>
+Created: <9/01/2026>
+Instructor: Holtslander
+"""
+
+q1 = input("Enter a noun: ")
+
+q2 = input("Enter a adjective: ")
+
+q3 = input("Enter a past-tense verb: ")
+
+q4 = input("Enter a plural noun: ")
+
+q5 = input("Enter a noun: ")
+
+q6 = input("Enter a adjective: ")
+
+q7 = input("Enter a place: ")
+
+q8 = input("Enter a verb ending with -ing: ")
+
+q9 = input("Enter a adjective: ")
+
+q10 = input("Enter a noun: ")
+
+q11 = input("Enter a plural noun: ")
+
+q12 = input("Enter a verb: ")
+
+q13 = input("Enter a adjective: ")
+
+q14 = input("Enter a noun: ")
+
+q15 = input("Enter a past tense verb: ")
+
+q16 = input("Enter a exclamation: ")
+
+q17 = input("Enter a verb ending with -ing: ")
+
+q18 = input("Enter a plural noun: ")
+
+q19 = input("Enter a adjective: ")
+
+q20 = input("Enter a noun: ")
+
+print (f"Once upon a time at a school in Manassas, a young {q1} walked into a very {q2} classroom. Suddenly, the teacher  {q3} onto the desk and shouted, Look at my new {q4}! Everyone grabbed their {q5} because it was going to be a {q6} day.")
+print (f"\nNext, the class took a field trip to the {q7}, where they saw a giant robot {q8}. The robot had a {q9} smile and held a sparkling {q10} in its hand. Suddenly, dozens of {q11} began to {q12} all over the floor! It was the most {q13} sight anyone had ever seen in the history of the {q14}.")
+print (f"\nThe principal {q15} through the doors and screamed, {q16}! Instead of studying, all the students started {q17}. In the end, the principal rewarded everyone with a pile of shiny {q18}. It was truly a {q19} adventure that no {q20} would ever forget!")
