1889585662242e598ffb41ef9ae48641cea228fd
jacob111764 <jacob111764@mcpsva.org>
2026-09-18 15:07:59 -0400

    .

---
+++ simple calculator.py
@@ -0,0 +1,14 @@
+print ("welcome to simple calculator")
+
+print ("\nthis is a simple calculator that will help you with 2 numbers on the 4 simple symbols")
+
+n1 = int(input ("\nenter first number: "))
+
+n2 = int(input ("\nenter second number: "))
+print (f"\nsum {n1+n2}")
+print (f"\ndifference {n1-n2}")
+print (f"\nproduct {n1*n2}")
+print (f"\nquotient {n1/n2}")
+
+print ("\nthank you for using simple caculator")
+print ("have a blessed day")
